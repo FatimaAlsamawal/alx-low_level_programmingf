48299ee8b456eebd85218872de6ca91f733a291c
FatimaAlsamawal <fatima.alsamawal@gmail.com>
2022-10-13 20:46:57 -0700

    4,5,6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void);
-
+ * main - Prints using puts() command*
+ * Return: 0 prints something
+ */
+int main(void)
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-
-	    return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
