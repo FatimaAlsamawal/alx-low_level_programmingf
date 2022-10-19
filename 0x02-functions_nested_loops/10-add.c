dc4ebfb256f1f693bcb5dc454b30ab685648d5e9
FatimaAlsamawal <113727964+FatimaAlsamawal@users.noreply.github.com>
2022-10-19 23:13:00 +0200

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Computes the sum of two input integers
+ * @n: first operant in the sum
+ * @m: secodn operant in the sum
+ *
+ * Return: the result of the sum "n + m"
+ */
+int add(int n, int m)
+{
+return (n + m);
+}
