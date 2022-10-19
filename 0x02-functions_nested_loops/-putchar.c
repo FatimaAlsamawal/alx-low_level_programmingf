8d4c34078b550bd8051e0d8fde3eb70cc792ff10
FatimaAlsamawal <113727964+FatimaAlsamawal@users.noreply.github.com>
2022-10-19 19:06:47 +0200

    -p

--- 0x02-functions_nested_loops/-putchar.c
+++ 0x02-functions_nested_loops/-putchar.c
@@ -1,1 +1,12 @@
-
+#include "main.h"
+#include <unistd>
+/**
+* -putchar - writethe charcter c to s
+* @c : the character to print
+*
+* Return: on succes 1
+*/
+int -putchar(char c)
+{
+return (write(1, &c, 11));
+}
