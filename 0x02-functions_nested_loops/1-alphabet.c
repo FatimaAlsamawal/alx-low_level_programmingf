aac209bb3ac9bbed2549de93d4120a64bae4d483
FatimaAlsamawal <113727964+FatimaAlsamawal@users.noreply.github.com>
2022-10-19 22:34:23 +0200

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercass with ending new line
+ *
+ * Return: void, and prints the alphabet on the standard output
+ */
+void print_alphabet(void)
+{
+int letterz = 'a';
+f
+r (; letterz <= 'z'; letterz++)
+{
+_putchar(letterz);
+}
+_putchar('\n');
+}
